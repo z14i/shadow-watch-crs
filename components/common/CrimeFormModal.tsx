f321bfbaa200c96ca2094002b8ebc5a17cfc3e04
Abdulrahim Yousuf <30768215+z14i@users.noreply.github.com>
2025-03-17 09:02:18 +0400

    small fix

--- components/common/CrimeFormModal.tsx
+++ components/common/CrimeFormModal.tsx
@@ -16,7 +16,8 @@ export default function CrimeFormModal({ onSubmit }: { onSubmit: (data: FormData
 
   return (
     <>
-      <Button onPress={onOpen} className="bg-blue-500 text-white px-4 py-2 rounded-md">
+      {/* Button to open the modal */}
+      <Button onClick={onOpen} className="bg-blue-500 text-white px-4 py-2 rounded-md">
         Report a Crime
       </Button>
       <Modal
@@ -24,15 +25,15 @@ export default function CrimeFormModal({ onSubmit }: { onSubmit: (data: FormData
         onOpenChange={onOpenChange}
         backdrop="opaque"
         classNames={{
-            body: "py-6",
-            backdrop: "bg-[#292f46]/50 backdrop-opacity-40",
-            base: "border-[#292f46] bg-[#19172c] dark:bg-[#19172c] text-[#a8b0d3]",
-            header: "border-b-[1px] border-[#292f46] ",
-            footer: "border-t-[1px] border-[#292f46]",
-            closeButton: "hidden",
+          container: "fixed inset-0 z-50 flex items-center justify-center",
+          backdrop: "bg-[#292f46]/50 backdrop-opacity-40",
+          base: "border-[#292f46] bg-[#19172c] dark:bg-[#19172c] text-[#a8b0d3]",
+          header: "border-b-[1px] border-[#292f46]",
+          footer: "border-t-[1px] border-[#292f46]",
+          closeButton: "hover:bg-white/5 active:bg-white/10 hidden",
         }}
       >
-        <ModalContent className="w-full max-w-2xl bg-black p-6 rounded-2xl flex flex-col ">
+        <ModalContent className="w-full max-w-4xl bg-black p-6 rounded-2xl flex flex-col">
           {(onClose) => (
             <>
               <ModalHeader className="text-xl font-bold mb-4">
@@ -41,8 +42,10 @@ export default function CrimeFormModal({ onSubmit }: { onSubmit: (data: FormData
               <ModalBody className="space-y-4">
                 <CrimeForm onClose={onClose} onSubmit={onSubmit} />
               </ModalBody>
-              <ModalFooter className="flex justify-end gap-2 mt-4">
-
+              <ModalFooter className=" justify-start gap-2 mt-4 hidden">
+                <Button onClick={onClose} color="danger" variant="light">
+                  Close
+                </Button>
               </ModalFooter>
             </>
           )}
